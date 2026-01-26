9474f476837eae2776ab1186f888254e5ef2b3fc
Team Firefli <team@firefli.com>
2026-01-26 13:30:14 +0000

    aa

--- components/livechat.tsx
+++ components/livechat.tsx
@@ -2,8 +2,8 @@ import Script from 'next/script';
 
 export default function LiveChat() {
   // Set to true and add your script src to enable livechat (customize locally, don't commit)
-  const enabled = false;
-  const scriptSrc = "";
+  const enabled = true;
+  const scriptSrc = "https://code.tidio.co/9ql5ypef9wk2tztxqnd972apfmng3hh1.js";
 
   if (!enabled || !scriptSrc) {
     return null;
